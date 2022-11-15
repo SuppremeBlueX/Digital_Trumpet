6e8d75b406f2b7829b722f23b92a22217b85e668
Charles <cpeters@southern.edu>
2022-11-15 15:40:33 -0500

    Adding samples again

---
+++ Digital_Trumpet_Proto_2.py
@@ -0,0 +1,63 @@
+import RPi.GPIO as GPIO
+import time
+from pygame import mixer
+
+GPIO.setmode(GPIO.BCM) # BOARD or BCM
+GPIO.setwarnings(False)
+
+# These pin values are the GPIO numbers, not the pin number
+valve1 = 16 # pin 36
+valve2 = 20 # pin 38
+valve3 = 21 # pin 40
+mouthpiece = 12 # pin 32
+
+buzzerpin = 19 # pin 35
+
+# GPIO setup
+GPIO.setup(valve1, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(valve2, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(valve3, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(mouthpiece, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(buzzerpin, GPIO.OUT)
+
+# Initialize Pygame Mixer
+mixer.init()
+
+# 
+sample_dir = "Trumpet_Samples/High_Quality/Long/Wav/"
+
+# Load the sample pitches (needed to be 16bit pcms)
+
+# ffmpeg -f s16le -ar 44.1k -ac 2 -i (old filename).wav (new filename).wav
+    # ffmpeg -f s16le -ar 44.1k -ac 2 -i C.wav C4_16bit.wav
+    # ffmpeg -f s16le -ar 44.1k -ac 2 -i C_sharp.wav C_sharp4_16bit.wav
+sound_c4 = mixer.Sound(f"{sample_dir}C4_16bit.wav")
+sound_csharp4 = mixer.Sound(f"{sample_dir}C_sharp4_16bit.wav")
+#sound_d4 = mixer.Sound(f"{sample_dir}D.wav")
+#sound_dsharp4 = mixer.Sound(f"{sample_dir}D_sharp.wav")
+#sound_e4 = mixer.Sound(f"{sample_dir}E.wav")
+#sound_f4 = mixer.Sound(f"{sample_dir}F.wav")
+#sound_fsharp4 = mixer.Sound(f"{sample_dir}F_sharp.wav")
+
+sound_c4.play()
+#sound_csharp4.play()
+
+try:
+    while True:
+        if not mixer.get_busy():
+            sound_c4.stop()
+            sound_c4.play()
+        if GPIO.input(mouthpiece) == GPIO.HIGH:
+            if GPIO.input(valve1) == GPIO.LOW and GPIO.input(valve2) == GPIO.LOW and GPIO.input(valve3) == GPIO.LOW:
+                print("c")
+                #sound_c4.play() # C (262 Hz)
+            elif GPIO.input(valve1) == GPIO.HIGH and GPIO.input(valve2) == GPIO.HIGH and GPIO.input(valve3) == GPIO.HIGH:
+                print("c#")
+                #sound_csharp4.play() # C# (278 Hz)
+        else:
+            sound_c4.stop()
+            sound_csharp4.stop()
+finally:
+    GPIO.cleanup()
+
+
