833a545ae902a1b7d4d384b66985bc262efab2fc
SuppremeBlueX <redpandac6@gmail.com>
2024-04-10 15:18:27 -0400

    More work on audio splitting

--- audio_split.py
+++ audio_split.py
@@ -3,44 +3,71 @@ from pydub import AudioSegment
 instrument = "Bb_Trumpet"
 mute = "Unmuted"
 
-def locate_peaks(sound):
+def find_indices(sound):
+	channels = 2
+	s_start_index = None
+	s_end_index = None
+	a_start_index = None
+	a_end_index = None
+	r_start_index = None
+	r_end_index = None
+	max_amp = False 
 	peak = 0
 	peaks = []
 	peaking = False
+	marking_attack = False
+	marking_sustain = False
+	marking_release = False
 	samples = sound.get_array_of_samples()
 	for index, sample in enumerate(samples):
-		if index % 2 == 0:
+		if index % channels == 0: # turning this from stereo to mono, insignificant change in quality
 			continue
 		else:
-			delta = samples[index] - samples[index-2] # delta change
+			delta = samples[index] - samples[index-channels] # delta change
 			if sample > 0 and delta < 0:
 				if peaking == False:
 					peak += 1
-					samples[index-2] = (2**15)-1
-					peaks.append(index-2)
+					# samples[index-channels] = (2**15)-1
+					peaks.append(index-channels)
 					peaking = True
+#					if sample == sound.max:
+#						max_amp = True
+#					if sample < (0.5 * sound.max) and (max_amp == False) and (marking_attack == False):
+#						marking_attack = True
+#						print(f"Attack Start Index: {index}")
+#						a_start_index = index
+#					elif marking_attack == True:
+#						a_end_index = index
+#						print(f"Attack End Index: {index}")
+#					if sample == (0.5 * sound.max):
+#						print(f"Sustain Start Index: {index}")
+#						marking_sustain = True
+#						s_start_index = index
+#					elif marking_sustain == True:
+#						print(f"Sustain End Index: {index}")
+#						s_end_index = index
+#						marking_sustain = False
+#					if sample == (0.5 * sound.max) and (max_amp == True):
+#						marking_release = True
+#						print(f"Release Start Index: {index}")
+#						r_start_index = index
 			elif sample < 0 and peaking == True:
 				peaking = False
-	return samples, peaks
-	
-def find_indices(sound,peaks):
+#		if marking_release == True:
+#			print(f"Release End Index: {index}")
+#			r_end_index = index
+#			marking_release = False
+			
+	print(len(peaks))
 	start_index = None
 	end_index = None
 	max_amplitude = sound.max
+	samples = sound.get_array_of_samples()
 	
-	s_start_index = peaks[2000]
-	s_end_index = peaks[2001]
-	a_start_index = peaks[0]
-	a_end_index = peaks[200]
-	r_start_index = peaks[-100]
-	r_end_index = peaks[-1]
-	
-	return s_start_index, s_end_index, a_start_index, a_end_index, r_start_index, r_end_index
+	return s_start_index, s_end_index, a_start_index, a_end_index, r_start_index, r_end_index, samples
 
 def sample(sound):
-	samples, peaks = locate_peaks(sound)
-	print(len(peaks))
-	s_start_index, s_end_index, a_start_index, a_end_index, r_start_index, r_end_index = find_indices(sound,peaks)
+	s_start_index, s_end_index, a_start_index, a_end_index, r_start_index, r_end_index, samples = find_indices(sound)
 	# export and extract sounds
 	extract = sound._spawn(samples[s_start_index:s_end_index])
 	extract.export(f"Samples/{instrument}/{mute}/Sound/Sustain/{file_name}_Sustain.wav", format="wav")
